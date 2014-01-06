36077b707c42a6163eaf03f5cd4fd1f4743bdf99
Jon Manning <jon@secretlab.com.au>
2014-01-06 20:16:49 +1100

    Convert DYNote to an NSManagedObject. Add the managed object model.

---
+++ Diary/Diary.m
@@ -0,0 +1,18 @@
+//
+//  Diary.m
+//  Diary
+//
+//  Created by Jon Manning on 6/01/2014.
+//  Copyright (c) 2014 Secret Lab. All rights reserved.
+//
+
+#import "Diary.h"
+
+
+@implementation Diary
+
+@dynamic text;
+@dynamic createdDate;
+@dynamic modifiedDate;
+
+@end
